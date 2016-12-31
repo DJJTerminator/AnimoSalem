688e112805bbaf483482a500cb13e78fe071c5f0
djjterminator <djjbionics93@gmail.com>
2016-12-31 00:58:18 -0600

    Programming Branch Remade

---
+++ Assets/Scripts/Objects/Objects/Mirror/Mirror.cs
@@ -0,0 +1,22 @@
+﻿// Finds out whether target is on the left or right side of the screen
+using UnityEngine;
+using System.Collections;
+
+public class Mirror : MonoBehaviour {
+	public Transform target;
+	public Camera _camera;
+	
+	void Start() {
+//		_camera = GetComponent<Camera>();
+	}
+	
+	void Update() {
+		Vector3 viewPos = GetComponent<Camera>().WorldToViewportPoint(target.position);
+//		if (viewPos.x > 0.5F)
+//			print("target is on the right side!");
+//		else
+//			print("target is on the left side!");
+
+		_camera.rect = new Rect(.6f,viewPos.y,0.06f,.6f);
+	}
+}
